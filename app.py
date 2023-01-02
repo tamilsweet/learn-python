3f9fb7f6b363655f4f23f3b716a176eea75ea97b
Tamilmozhi Gunasekar <tgunasekar@rbbn.com>
2023-01-02 21:34:31 +0530

    Get store, get items in store API's

--- app.py
+++ app.py
@@ -15,31 +15,47 @@ stores = [
 ]
 
 
-@app.get('/store')
+@app.get("/store")
 def get_stores():
   return {"stores": stores}
 
 
-@app.post('/store')
+@app.post("/store")
 def create_store():
   request_data = request.get_json()
   new_store = {
-      "name": request_data['name'],
+      "name": request_data["name"],
       "items": []
   }
   stores.append(new_store)
   return new_store, 201
 
 
-@app.post('/store/<string:name>/item')
+@app.post("/store/<string:name>/item")
 def add_item(name):
   request_data = request.get_json()
   for store in stores:
-    if store['name'] == name:
+    if store["name"] == name:
       new_item = {
-          "name": request_data['name'],
-          "price": request_data['price']
+          "name": request_data["name"],
+          "price": request_data["price"]
       }
       store["items"].append(new_item)
       return new_item, 201
-  return { "message": "Store not found" }, 404
+  return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>")
+def get_store(name):
+  for store in stores:
+    if store["name"] == name:
+      return {"store": store}
+  return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>/item")
+def get_items_in_store(name):
+  for store in stores:
+    if store["name"] == name:
+      return {"items": store["items"]}
+  return {"message": "Store not found"}, 404
