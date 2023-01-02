1c07e5e1b2e2d4c9b8f17beba1b43fd037a67c57
Tamilmozhi Gunasekar <tgunasekar@rbbn.com>
2023-01-02 21:26:42 +0530

    new endpoints: Add store, Add items

--- app.py
+++ app.py
@@ -18,3 +18,27 @@ stores = [
 @app.get('/store')
 def get_stores():
   return {"stores": stores}
+
+
+@app.post('/store')
+def create_store():
+  request_data = request.get_json()
+  new_store = {
+      "name": request_data['name'],
+      "items": []
+  }
+  stores.append(new_store)
+  return new_store, 201
+
+
+@app.post('/store/<string:name>')
+def add_item(name):
+  request_data = request.get_json()
+  for store in stores:
+    if store['name'] == name:
+      new_item = {
+          "name": request_data['name'],
+          "price": request_data['price']
+      }
+      store["items"].append(new_item)
+      return new_item, 201
