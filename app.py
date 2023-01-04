5cf7fc1042206a78147fd5854f1b47f00f472f28
Tamilmozhi Gunasekar <tgunasekar@rbbn.com>
2023-01-04 09:50:20 +0530

    Add validations to store and item creation

--- app.py
+++ app.py
@@ -14,6 +14,14 @@ def get_stores():
 @app.post("/store")
 def create_store():
   store_data = request.get_json()
+  # Check if required fields are present
+  if "name" not in store_data:
+    abort(400, message="Bad Request. Ensure 'name' is included in the JSON payload.")
+
+  for store in stores.values():
+    if store["name"] == store_data["name"]:
+      abort(400, message="Store already exists.")
+
   store_id = uuid.uuid4().hex
   store = {
       **store_data,
@@ -26,6 +34,19 @@ def create_store():
 @app.post("/item")
 def add_item():
   item_data = request.get_json()
+  # Check if required fields are present
+  if (
+    "price" not in item_data
+    or "store_id" not in item_data
+    or "name" not in item_data
+  ):
+    abort(400, message="Bad Request. Ensure 'price', 'store_id', and 'name' are included in JSON payload.")
+
+  # Check if item already exist in same store
+  for item in items.values():
+    if item["name"] == item_data["name"] and item["store_id"] == item_data["store_id"]:
+      abort(400, message="Item already exists")
+
   if item_data["store_id"] not in stores:
     abort(404, message="Store not found")
 
