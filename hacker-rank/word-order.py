eae3f9944c70e0ed9d87675ef0f39d7bea7b61c0
Tamilmozhi Gunasekar <tgunasekar@rbbn.com>
2023-01-14 09:23:08 +0530

    optimize: word-order

--- hacker-rank/word-order.py
+++ hacker-rank/word-order.py
@@ -9,3 +9,13 @@ for _ in range(n):
 
 print(len(words))
 print(*words.values())
+
+##############
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import Counter
+
+n = int(input())
+words = Counter([input() for _ in range(n)])
+print(len(words))
+print(*words.values())
