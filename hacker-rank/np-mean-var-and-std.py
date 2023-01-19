85c2aa820aded06fe57951ad3ffd5f7f56166758
Tamilmozhi Gunasekar <tgunasekar@rbbn.com>
2023-01-19 21:28:00 +0530

    Challenge: np-mean-var-and-std

---
+++ hacker-rank/np-mean-var-and-std.py
@@ -0,0 +1,7 @@
+import numpy
+
+N, M = map(int, input().split())
+A = numpy.array([input().split() for _ in range(N)], int)
+print(numpy.mean(A, axis = 1))
+print(numpy.var(A, axis = 0))
+print(numpy.around(numpy.std(A, axis = None), decimals=11))
