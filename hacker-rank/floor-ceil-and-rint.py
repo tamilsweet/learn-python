4ba5bec0a5eda8d5fb148da354513ab60b50ed2a
Tamilmozhi Gunasekar <tgunasekar@rbbn.com>
2023-01-19 21:12:52 +0530

    Optimize: floor-ceil-and-rint

--- hacker-rank/floor-ceil-and-rint.py
+++ hacker-rank/floor-ceil-and-rint.py
@@ -1,7 +1,7 @@
 import numpy
 numpy.set_printoptions(legacy='1.13')
 
-A = numpy.array(list(map(float, input().split())))
+A = numpy.array(input().split(), float)
 print(numpy.floor(A))
 print(numpy.ceil(A))
 print(numpy.rint(A))
