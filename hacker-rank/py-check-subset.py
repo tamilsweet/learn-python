465962b8f3e35e1969ab74602c12e70d440ac50c
Tamilmozhi Gunasekar <tgunasekar@rbbn.com>
2023-01-20 20:50:46 +0530

    Optimize: py-check-subset

--- hacker-rank/py-check-subset.py
+++ hacker-rank/py-check-subset.py
@@ -2,13 +2,6 @@
 
 T = int(input())
 for _ in range(T):
-    a_count = int(input())
-    A = list(map(int, input().split()))
-    b_count = int(input())
-    B = list(map(int, input().split()))
-    subset = 'True'
-    for a in A:
-        if a not in B:
-            subset = 'False'
-            break
-    print(subset)
+    _, A = input(), set(map(int, input().split()))
+    _, B = input(), set(map(int, input().split()))
+    print(A.issubset(B))
